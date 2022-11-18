6a40017216753d4afc5a5c905db715b8dba4a5a2
BrFern <102929013+BrFern@users.noreply.github.com>
2022-11-18 15:55:01 -0700

    Second video blank code

--- script.js
+++ script.js
@@ -18,25 +18,46 @@
 
 //Helpful to do in the background
 
-const recordVideoOne = new Promise ((resolve, reject) => {
-    resolve('Video 1 Recorded')
-})
+// const recordVideoOne = new Promise ((resolve, reject) => {
+//     resolve('Video 1 Recorded')
+// })
 
-const recordVideoTwo = new Promise ((resolve, reject) => {
-    resolve('Video 2 Recorded')
-})
+// const recordVideoTwo = new Promise ((resolve, reject) => {
+//     resolve('Video 2 Recorded')
+// })
 
-const recordVideoThree = new Promise ((resolve, reject) => {
-    resolve ('Video 3 Recorded')
-})
+// const recordVideoThree = new Promise ((resolve, reject) => {
+//     resolve ('Video 3 Recorded')
+// })
 
-//Running all the promises
-Promise.race([
-    recordVideoOne,
-    recordVideoTwo,
-    recordVideoThree
-]).then((message)=> {
-    console.log(message)
-}) 
+// //Running all the promises
+// Promise.race([
+//     recordVideoOne,
+//     recordVideoTwo,
+//     recordVideoThree
+// ]).then((message)=> {
+//     console.log(message)
+// }) 
 //.then will send an array of all the messages.
 //Promise.all called all, Promise.race will only return a single message
+
+
+//ASYNC AWAIT
+
+function makeRequest(location) {
+    return new Promise((resolve, reject) => {
+        console.log(`Making Request to ${location}`)
+        if (location === 'Google') {
+            resolve('Google says hi')
+        } else {
+            reject('We can only tlk to Google')
+        }
+    })
+}
+
+function processRequest(response) {
+    return new Promise((resolve, reject) => {
+        console.log('Processing response')
+        resolve(`Extra Information + ${response}`)
+    })
+}
