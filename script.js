ecd0bcc31f7f27c85cb3c91114bff5c82a728a1c
BrFern <102929013+BrFern@users.noreply.github.com>
2022-11-18 16:09:47 -0700

    Finished Async video

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function makeRequest(location) {
         if (location === 'Google') {
             resolve('Google says hi')
         } else {
-            reject('We can only tlk to Google')
+            reject('We can only talk to Google')
         }
     })
 }
@@ -61,3 +61,35 @@ function processRequest(response) {
         resolve(`Extra Information + ${response}`)
     })
 }
+
+//If requesting something besides Google it will go to the error part
+// makeRequest('Google').then(response => {
+//     console.log('Response Received')
+//     return processRequest(response)
+// }).then(processdResponse => {
+//     console.log(processdResponse)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+//Need some kind of function that your awaiting code is inside of 
+async function doWork() {
+    try {
+   const response = await makeRequest('Facebook') //tells code to wait with await keyword
+   console.log('Response Received')
+   const processedResponse = await processRequest(response)
+   console.log(processedResponse)
+} catch (err) {
+    console.log(err)
+}
+
+}
+doWork()
+
+//How to get error--> use try-put in all the code that could potentially
+//fail Any errors that come through the try section will go into the 
+//catch section 
+
+//Much easier to reason with
+//Biggest thing-make sure to have it wrapped in a function and
+//has async 
